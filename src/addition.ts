58edfa713c7f67e2a14f8cca1263fd5bf09640b1
iy88 <1845767798@qq.com>
2021-06-05 21:49:35 +0800

    update

--- src/addition.ts
+++ src/addition.ts
@@ -1,30 +1,27 @@
-// let additionMap: { [k: string]: { [k: string]: { carry: boolean, res: string, R?: { res: string, r: string } } } } = {};
-// for (let i = 0; i < 10; i++) {
-//   additionMap[`${i}`] = { '0': { carry: false, res: `${i}` } };
-//   for (let j = 1; j < 10; j++) {
-//     let res = i + j;
-//     additionMap[`${i}`][`${j}`] = { carry: res > 9 ? true : false, res: `${res}`, R: res > 9 ? { res: `${res - 10}`, r: `1` } : undefined };
-//   }
-// }
-import additionMap from "./data/additionmap";
+let additionMap: { [k: string]: { [k: string]: { carry: boolean, res: string, R?: { res: string, r: string } } } } = {};
+for (let i = 0; i < 10; i++) {
+  additionMap[`${i}`] = { '0': { carry: false, res: `${i}` } };
+  for (let j = 1; j < 10; j++) {
+    let res = i + j;
+    additionMap[`${i}`][`${j}`] = { carry: res > 9 ? true : false, res: `${res}`, R: res > 9 ? { res: `${res - 10}`, r: `1` } : undefined };
+  }
+}
+// import additionMap from "./data/additionmap";
 function addition(a: string, b: string) {
   if (!(isNaN(Number(a)) || isNaN(Number(b)))) {
-    let aa: string[] = a.split('');
-    let ba: string[] = b.split('');
-    while (aa.length - (aa.indexOf('.') + 1) !== ba.length - (ba.indexOf('.') + 1)) {
-      if (aa.length - (aa.indexOf('.') + 1) > ba.length - (ba.indexOf('.') + 1)) {
-        ba.push('0');
-      } else {
-        aa.push('0');
-      }
-    }
+    let aa = a;
+    let ba = b;
+    const fillLena = aa.slice(aa.indexOf('.')).length;
+    const fillLenb = ba.slice(ba.indexOf('.')).length;
+    let c = fillLena - fillLenb;
+    let d = fillLenb - fillLena
+    const maxLena = c < 0 ? d + aa.length : aa.length;
+    const maxLenb = d < 0 ? c + ba.length : ba.length;
+    aa = aa.padEnd(maxLena, '0');
+    ba = ba.padEnd(maxLenb, '0');
     let maxLength = Math.max(aa.length, ba.length) + 1;
-    while (aa.length !== maxLength) {
-      aa.unshift('0');
-    }
-    while (ba.length !== maxLength) {
-      ba.unshift('0');
-    }
+    aa = aa.padStart(maxLength, '0');
+    ba = ba.padStart(maxLength, '0');
     let res: string[] = []
     for (let i = maxLength - 1; i > -1; i--) {
       let n1 = aa[i];
@@ -34,62 +31,70 @@ function addition(a: string, b: string) {
         res[i] = '.';
         continue;
       }
-      if (additionMap[n1][n2].carry) {
+      let addRes = additionMap[n1][n2];
+      if (addRes.carry) {
         if (r1) {
-          let tmp = additionMap[n1][n2].R!.res;
-          let R1 = additionMap[n1][n2].R!.r;
-          if (additionMap[r1][tmp].carry) {
-            let R2 = additionMap[r1][tmp].R!.r;
-            res[i] = additionMap[r1][tmp].R!.res;
-            if (additionMap[R1][R2].carry) {
-              if (aa[i - 2] !== '.') {
-                res[i - 2] = additionMap[R1][R2].R!.r;
+          let tmp = addRes.R!.res;
+          let R1 = addRes.R!.r;
+          let tmpRes = additionMap[r1][tmp];
+          if (tmpRes.carry) {
+            let R2 = tmpRes.R!.r;
+            res[i] = tmpRes.R!.res;
+            let R = additionMap[R1][R2];
+            let resR = R.res;
+            let RR = R.R!.r;
+            let last = aa[i - 1];
+            let lastLast = aa[i - 2];
+            if (R.carry) {
+              if (lastLast !== '.') {
+                res[i - 2] = RR;
               } else {
-                res[i - 3] = additionMap[R1][R2].R!.r;
+                res[i - 3] = RR;
               }
-              if (aa[i - 1] !== '.') {
-                res[i - 1] = additionMap[R1][R2].R!.res;
+              if (last !== '.') {
+                res[i - 1] = resR;
               } else {
-                res[i - 2] = additionMap[R1][R2].R!.res;
+                res[i - 2] = resR;
               }
             } else {
-              if (aa[i - 1] !== '.') {
-                res[i - 1] = additionMap[R1][R2].R!.res;
+              if (last !== '.') {
+                res[i - 1] = resR;
               } else {
-                res[i - 2] = additionMap[R1][R2].R!.res;
+                res[i - 2] = resR;
               }
             }
           } else {
             if (aa[i - 1] !== '.') {
-              res[i - 1] = additionMap[n1][n2].R!.r;
+              res[i - 1] = addRes.R!.r;
             } else {
-              res[i - 2] = additionMap[n1][n2].R!.r;
+              res[i - 2] = addRes.R!.r;
             }
-            res[i] = additionMap[r1][tmp].res;
+            res[i] = tmpRes.res;
           }
         } else {
           if (aa[i - 1] !== '.') {
-            res[i - 1] = additionMap[n1][n2].R!.r;
+            res[i - 1] = addRes.R!.r;
           } else {
-            res[i - 2] = additionMap[n1][n2].R!.r;
+            res[i - 2] = addRes.R!.r;
           }
-          res[i] = additionMap[n1][n2].R!.res;
+          res[i] = addRes.R!.res;
         }
       } else {
         if (r1) {
           let tmp = additionMap[n1][n2].res;
-          if (additionMap[r1][tmp].carry) {
-            res[i] = additionMap[r1][tmp].R!.res;
+          let tmpRes = additionMap[r1][tmp]
+          if (tmpRes.carry) {
+            res[i] = tmpRes.R!.res;
             if (aa[i - 1] !== '.') {
-              res[i - 1] = additionMap[r1][tmp].R!.r;
+              res[i - 1] = tmpRes.R!.r;
             } else {
-              res[i - 2] = additionMap[r1][tmp].R!.r;
+              res[i - 2] = tmpRes.R!.r;
             }
           } else {
-            res[i] = additionMap[r1][tmp].res;
+            res[i] = tmpRes.res;
           }
         } else {
-          res[i] = additionMap[n1][n2].res;
+          res[i] = addRes.res;
         }
       }
     }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
 import addition from "./addition";
-console.log(addition('0.1','0.2'));
+let s = new Array(100000).fill(9).join('');
+console.time()
+addition(s,s);
+console.timeEnd();
+console.time()
+addition(s,s);
+console.timeEnd();
+// console.log(addition('0,1','0.2'))
